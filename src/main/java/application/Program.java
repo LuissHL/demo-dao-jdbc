a201f24b465b3055bed57961ec48f3131b63ddcd
Luis henrique <105991145+LuissHL@users.noreply.github.com>
2025-05-13 15:29:36 -0300

    findById implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,4 +1,15 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Seller;
+
 public class Program {
+    public static void main(String[] args) {
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+    }
 }
